cc5ac1c728f6dcc3e803be728f1fcee6d70d320a
Alex Vieira <alex.vieira30@etec.sp.gov.br>
2021-01-06 18:12:28 -0300

    Fetching data from backend

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
+import { Product } from "./types";
+import { fetchProducts } from "../api";
 import StepsHeader from "./StepsHeader";
+import ProductsList from "./ProductsList";
 import "./styles.css";
 
 function Orders() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetchProducts()
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className='orders-container'>
       <StepsHeader />
+      <ProductsList products={products} />
     </div>
   );
 }
